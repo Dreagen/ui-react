fb0e7933a754faa3701fe5212ff30393ee8482f3
Nicholas Allen <nicholas.allen@mhr.co.uk>
2020-10-23 10:40:47 +0100

    Style course list

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,49 +1,54 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Container } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableRow: {
+    cursor: "pointer"
+  }
+});
 
 function CourseList(props) {
+  const classes = useStyles();
+  const history = useHistory();
+
   return (
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title teal-text text-lighten-3">Courses</span>
-            <table>
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Author ID</th>
-                  <th>Category</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {props.courses.map((course) => {
-                  return (
-                    <tr key={course.id}>
-                      <td>{course.title}</td>
-                      <td>{course.authorId}</td>
-                      <td>{course.category}</td>
-                      <td>
-                        <Link className="btn" to={"/course/" + course.slug}>
-                          Manage
-                        </Link>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          <div class="card-action">
-            <Link to="/course" className="btn btn-primary">
-              Add Course
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container maxWidth="md">
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Author ID</TableCell>
+            <TableCell align="right">Category</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.courses.map((course) => (
+            <TableRow className={classes.tableRow} hover key={course.id} onClick={() => history.push("course")}>
+              <TableCell component="th" scope="row">
+                {course.title}
+              </TableCell>
+              <TableCell align="right">{course.authorId}</TableCell>
+              <TableCell align="right">{course.category}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Container>
   );
 }
 
