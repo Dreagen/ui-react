a1d8fc36d7f9a867c45434e9613f29613b729308
Nicholas Allen <nicholas.allen@mhr.co.uk>
2020-10-23 11:36:44 +0100

    Add image to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container } from "@material-ui/core";
+import { CardHeader, CardMedia, Container } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import reactImage from '../static/react.jpeg';
 
 function AboutPage(props) {
   const useStyles = makeStyles((theme) => ({
@@ -15,20 +16,23 @@ function AboutPage(props) {
     title: {
       fontSize: 16,
     },
+    media: {
+      height: 0,
+      paddingTop: "30.00%", // 16:9
+    },
   }));
   const classes = useStyles();
 
   return (
     <Container maxWidth="md">
       <Card className={classes.root}>
+        <CardHeader title="About" />
+        <CardMedia
+          className={classes.media}
+          image={reactImage}
+          title="react"
+        />
         <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            About
-          </Typography>
           <Typography variant="body2" component="p">
             This app uses React
           </Typography>
